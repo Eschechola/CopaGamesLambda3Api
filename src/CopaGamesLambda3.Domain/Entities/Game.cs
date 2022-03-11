d952bc9b5f42edcd5777ade431ad0bf27a90a271
Lucas Eschechola <lucas.eschechola@outlook.com>
2022-03-11 18:12:19 -0300

    Add domain entities

---
+++ src/CopaGamesLambda3.Domain/Entities/Game.cs
@@ -0,0 +1,25 @@
+﻿namespace CopaGamesLambda3.Domain.Entities
+{
+    public class Game
+    {
+        public string Id { get; private set; }
+        public string Title { get; private set; }
+        public double Rating { get; private set; }
+        public int Year { get; private set; }
+        public string UrlImage { get; private set; }
+
+        public Game(
+            string id,
+            string title,
+            double rating,
+            int year,
+            string urlImage)
+        {
+            Id = id;
+            Title = title;
+            Rating = rating;
+            Year = year;
+            UrlImage = urlImage;
+        }
+    }
+}
