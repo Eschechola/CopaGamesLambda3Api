24cd0071f93c3e925e463b00428928eb87085116
Lucas Eschechola <lucas.eschechola@outlook.com>
2022-03-11 18:12:59 -0300

    Add infrasturcture and games API request

---
+++ src/CopaGamesLambda3.Infrastructure/Communication/Refit/GameResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace CopaGamesLambda3.Infrastructure.Communication.Refit
+{
+    public class GameResponse
+    {
+        public string Id { get; set; }
+        public string Titulo { get; set; }
+        public double Nota { get; set; }
+        public int Ano { get; set; }
+        public string UrlImagem { get; set; }
+    }
+}
---
+++ src/CopaGamesLambda3.Infrastructure/Communication/Refit/IGameApi.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CopaGamesLambda3.Infrastructure.Communication.Refit
+{
+    public interface IGameApi
+    {
+        [Get("/Competidores?copa=games")]
+        Task<IList<GameResponse>> GetGamesAsync();
+    }
+}
